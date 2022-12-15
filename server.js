488c5b377802524c0080f72b2acc978f79b46113
skdejong <stephaniekdejong@gmail.com>
2022-12-15 19:21:20 +0000

    Added comments to the code

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.listen(PORT, function () {
 
 
 /* 
-The code you have shared is a server file for a web app. 
-It imports the main app file and then use the app.listen method to start the server, listening on the port specified in the process.env.PORT variable. 
+This is a server file for a web app. 
+It imports the main app file and then use the app.listen method to start the server, 
+listening on the port specified in the process.env.PORT variable. 
 The console.log statement is used to print a message to the console indicating that the server is running and on which port it is listening.
 */
